98d05d6b438f6b2738236e98f0a8216692c0af17
Jhutus <67081090+Jhutus@users.noreply.github.com>
2025-03-13 18:30:31 -0500

    Fixed scroll with buttons
    Js code fixed.

--- script.js
+++ script.js
@@ -3,7 +3,15 @@ document.querySelectorAll('nav a').forEach(anchor => {
         e.preventDefault();
         const targetId = this.getAttribute('href').substring(1);
         const targetSection = document.getElementById(targetId);
-        
-        targetSection.scrollIntoView({ behavior: 'smooth' });
+        const headerHeight = document.querySelector('header').offsetHeight; // Altura del header
+
+        // Calcula la posición de la sección restando la altura del header
+        const offsetPosition = targetSection.offsetTop - headerHeight;
+
+        // Realiza el scroll con el offset
+        window.scrollTo({
+            top: offsetPosition,
+            behavior: 'smooth'
+        });
     });
 });
